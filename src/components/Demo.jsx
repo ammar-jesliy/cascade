a22552032870e32f89f4257468c6e87edc097d68
ammar-jesliy <ammarjc1@gmail.com>
2024-06-19 09:37:23 +0530

    Edit demo

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Demo = ({ image, left }) => {
   return (
-    <div className="w-full h-[400px] bg-accent1 rounded-3xl aspect-[6/4] md:w-1/2 overflow-hidden">
+    <div className="w-full bg-accent1 rounded-3xl aspect-[6/4] md:w-1/2 overflow-hidden">
       <img src={image} alt="demo" 
         className={`w-full h-full object-cover ${left && "object-left"}`}
       />
